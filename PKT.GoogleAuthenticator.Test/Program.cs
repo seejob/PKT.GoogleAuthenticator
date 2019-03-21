a6d9a2457873a705f909f59327e0c97766a18cd3
bin58200 <547959392@qq.com>
2019-03-21 18:14:28 +0800

    Create README.md

---
+++ PKT.GoogleAuthenticator.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using PKT.GoogleAuthenticator.Test.Utils;
+using System;
+
+namespace PKT.GoogleAuthenticator.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountTitle = "account";
+
+            var secretKey = "f68f1fe894d548a1bbc66165c46e61eb"; //Guid.NewGuid().ToString().Replace("-", "");
+
+           var ImageUrl= AuthenticatorUtil.Generate(accountTitle, secretKey);
+            
+            Console.WriteLine(ImageUrl);
+        }
+    }
+}
